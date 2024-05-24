22cd1790dd9923826db20798407d906ca5c16396
julioceaseless <julioceaseless@gmail.com>
2024-05-24 08:03:58 +0000

    add doc_string and comments

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+initialize the models package
+"""
 from os import environ
 
-# get the storage type
+
+# get the storage type from environment
 storage_type = environ.get('APPIARY_TYPE_STORAGE')
 
-
+# select engine based on the storage type
 if storage_type == "db":
     from models.engine.db_storage import DBStorage
     storage = FileStorage()
@@ -12,5 +16,5 @@ else:
     from models.engine.file_storage import FileStorage
     storage = DBStorage()
 
-
+# reload objects from storage
 storage.reload()
