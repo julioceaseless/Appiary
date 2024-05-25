1fb32ea9dff2260f1dadc60b83f8269092d60002
julioceaseless <julioceaseless@gmail.com>
2024-05-25 08:42:26 +0000

    Add sqlalchemy for ORM

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,28 @@
 This is a parent model
 '''
 import uuid
+import models
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+from os import environ
 
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
+
+if environ.get("APPIARY_TYPE_STORAGE") == "db":
+    Base = declarative_base()
+else:
+    Base = object
+
+
 class BaseModel:
     """This is the base model"""
+
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=created_at)
+
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
         if kwargs:
@@ -34,6 +50,7 @@ class BaseModel:
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
 
+    
     def to_dict(self):
         """serialize an object to dictionary"""
         # retrieve dictionary representation of the object
@@ -46,12 +63,24 @@ class BaseModel:
         return obj_to_dict
 
 
+    def delete(self):
+        """
+        delete the current instance from storage
+        """
+        models.storage.delete(self)
+
+
+    def save(self):
+        """
+        save and update by updating 'updated_at' attribute to
+        current time
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+
+
     def __str__(self):
         """Return String representation of the object"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.to_dict())
-
-    
-    def update(self):
-        """save and update 'updated_at' the time to current time"""
-        self.updated_at = datetime.utcnow()
