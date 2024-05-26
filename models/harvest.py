0b066002ab98a9ab0afd4458bd3cff430005a66e
julioceaseless <julioceaseless@gmail.com>
2024-05-26 03:02:03 +0000

    Add sqlalchemy Override UUID with integer ID Make pep8 compliant

--- models/harvest.py
+++ models/harvest.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
-"""Harvests"""
+"""Harvest Model"""
 
-from datetime import datetime
-from models.base_model import BaseModel
-from models.beehive import Beehive
+
 import models
+from models.base_model import BaseModel
+import sqlalchemy
+from sqlalchemy import Column, String, Float, ForeignKey
+from datetime import datetime
+
 
 class Harvest(BaseModel):
-    """ record honey harvests"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        valid_keys = ['hive_id', 'quantity', 'notes']
-        valid_kwargs = {key: value for key, value in kwargs.items() if key in valid_keys}
-        self.__dict__.update(valid_kwargs)
+    """Define harvest class"""
+    _last_id = 0
 
-    
+    if models.storage_type == 'db':
+        # use database storage
+        __tablename__ = 'harvests'
+
+        # override the UUID column with integer ID
+        id = Column(Integer, primary_key=True)
+
+        hive_id = Column(String(60), ForeignKey('beehives.id'), nullable=False)
+        quantity = Column(Float, default=0.00)
+        notes = Column(String(1024), nullable=True)
+    else:
+        # use file storage
+        hive_id = ''
+        quantity = 0.00
+        notes = ''
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Harvest"""
+        super().__init__(*args, **kwargs)
+        if not kwargs.get('id'):
+            Harvest._last_id += 1
+            self.id = Harvest._last_id
+        else:
+            self.id = kwargs['id']
+
     def next_harvest(self):
         """modifies the Beehive object to schedule the next harvest date"""
         beehive = models.storage.get("Beehive", self.hive_id)
@@ -27,8 +50,11 @@ class Harvest(BaseModel):
                 current_month = datetime.now().month
                 next_month = (current_month + 3) % 12
                 next_year = datetime.now().year + (current_month + 3) / 12
-                next_harvest_date = datetime(int(next_year), int(next_month), int(self.created_at.day)).isoformat()
-                
+                next_harvest_date = datetime(int(next_year),
+                                             int(next_month),
+                                             int(self.created_at.day)
+                                             ).isoformat()
+
                 # set next harvest date
                 if hasattr(beehive, 'next_harvest_date'):
                     beehive.next_harvest_date = next_harvest_date
@@ -39,12 +65,3 @@ class Harvest(BaseModel):
                 return f"{beehive.id} is not ready for harvest!"
         else:
             return "Error: Beehive not found."
-
-
-if __name__ == "__main__":
-    beehive = Beehive("1234")
-    harvest = Harvest(beehive)
-    print(harvest.__dict__)
-    print("---------------beehive--------------------")
-    print(beehive.__dict__)
-    
