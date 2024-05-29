fc0817aea6ea08f9afe55158ea8f9b5b5d2b71e8
julioceaseless <julioceaseless@gmail.com>
2024-05-29 12:45:59 +0000

    small changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,15 @@ from flask import jsonify
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Beehive, Harvest, Inspection, User, Apiary]
+    names = ["beehives", "harvests", "inspections", "users", "apiaries"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
