e505e7f46c3772a1971be6e69bc016a6fcaaa087
julioceaseless <julioceaseless@gmail.com>
2024-05-30 08:39:37 +0000

    Add end points for - harvests - beehives - inspections

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,6 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.apiaries import *
+from api.v1.views.beehives import *
+from api.v1.views.harvests import *
+from api.v1.views.inspections import *
