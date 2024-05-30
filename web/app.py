4168bb2b8fd14423866ed9ee095b85708d488089
julioceaseless <julioceaseless@gmail.com>
2024-05-30 14:59:25 +0000

    web flask

---
+++ web/app.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+'''Start a flask web application'''
+from models import storage
+from os import environ
+from flask import Flask, render_template, request, redirect, url_for
+from models.apiary import Apiary
+from models.beehive import Beehive
+
+# start flask
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/apiaries')
+def list_apiaries():
+    apiaries = storage.all('Apiary')
+    return render_template('list_apiaries.html', apiaries=apiaries)
+
+@app.route('/apiary/<str:apiary_id>')
+def view_apiary(apiary_id):
+    apiary = storage.get('Apiary', apiary_id)
+    return render_template('view_apiary.html', apiary=apiary)
+
+@app.route('/apiary/add', methods=['GET', 'POST'])
+def add_apiary():
+    if request.method == 'POST':
+        name = request.form['name']
+        location = request.form['latitude']
+        new_apiary = Apiary(name=name, latitude=latitude)
+        new_apiary.save()
+        return redirect(url_for('list_apiaries'))
+    return render_template('add_apiary.html')
+
+@app.route('/apiary/delete/<int:apiary_id>', methods=['POST'])
+def delete_apiary(apiary_id):
+    apiary = Apiary.query.get_or_404(apiary_id)
+    db.session.delete(apiary)
+    db.session.commit()
+    return redirect(url_for('list_apiaries'))
+
+if __name__ == '__main__':
+    """Main function"""
+
+    # get host
+    if environ.get('HOST'):
+        HOST = environ.get('HOST')
+    else:
+        HOST = '0.0.0.0'
+
+    # get port
+    if environ.get('PORT'):
+        PORT = environ.get('PORT')
+    else:
+        PORT = 5555
+
+    app.run(host=HOST, port=PORT, debug=True)
+
