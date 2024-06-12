b659912efe5c915c875e31a9cfb2b4fdd466627c
julioceaseless <julioceaseless@gmail.com>
2024-06-12 01:23:08 +0000

    Add password authentication

--- web/views/auth.py
+++ web/views/auth.py
@@ -12,16 +12,41 @@ from decorators import login_required
 
 @views.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
+    """
+    Handles sign up process.
+    POST method handles sign up data in the signup form
+    GET method displays the form for user to create account
+    """
     if request.method == "POST":
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
         email = request.form.get('email')
         yob = request.form.get('yob')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-        if not first_name or not last_name or not email or not yob:
-            flash("All fields are required!")
+        # check empty fields
+        if (not first_name or
+                not last_name or
+                not email or
+                not yob or
+                not password1 or
+                not password2):
+            flash("All fields are required!", category="error")
             return redirect(url_for('views.sign_up'))
 
+        # check if email exists
+        email_exists = storage.query(User).filter_by(email=email).first()
+
+        # catch errors
+        if email_exists:
+            flash("Email is already in use", category="error")
+        elif password1 != password2:
+            flash("Passwords don\'t match!", category="error")
+        elif len(password) < 5:
+            flash("Password too short!", category="error")
+
+        # calculate age
         try:
             yob = int(yob)
         except ValueError:
@@ -32,12 +57,16 @@ def sign_up():
             'last_name': last_name,
             'email': email,
             'yob': yob
+            'password': password1
             }
-
+        
+        # create new user
         user = User(**data)
         user.save()
 
+        # Set session id
         session['user_id'] = user.id
+
         return redirect(url_for('views.show_profile'))
 
     return render_template("sign-up.html")
@@ -60,18 +89,28 @@ def login():
     """Login user"""
     if request.method == "POST":
         email = request.form.get('email')
+        password = request.form.get('password')
 
-        if not email:
-            flash("Email is required!")
+        if not email or not password:
+            flash("Password or Email is missing!")
             return redirect(url_for('views.login'))
 
         # Optimized search for the user by email
         user = storage.query(User).filter_by(email=email).first()
         if user:
-            session['user_id'] = user.id
-            return redirect(url_for('views.show_profile'))
-
-        flash("Invalid email or user does not exist")
+            # check if password is correct
+            if user.password == password:
+                # set session id
+                session['user_id'] = user.id
+                # show login success notification
+                flash("Logged in!", category="success")
+                # show user profile
+                return redirect(url_for('views.show_profile'))
+            else:
+                flash("Password incorrect!", category="error")
+                return redirect(url_for('views.login'))
+        
+        flash("Invalid email or user does not exist", category="error")
         return redirect(url_for('views.login'))
 
     return render_template("login.html")
