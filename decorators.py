12877f07fb1909c994f47db58497524a2073ae98
julioceaseless <julioceaseless@gmail.com>
2024-06-14 06:22:10 +0000

    Fix token validation

--- decorators.py
+++ decorators.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Appiary/decorators.py """
 from functools import wraps
-from flask import session, redirect, url_for, flash
+from flask import session, jsonify, redirect, url_for, flash, current_app, request
+from models import storage
+from models.user import User
+import jwt
 
 
 def login_required(f):
@@ -28,13 +31,18 @@ def token_required(f):
     """
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.headers.get('Authorization')
+        token = request.headers.get('Authorization').split()[1]
+        secret_key = request.headers.get('Key')
+
         if not token:
             return jsonify({'message': 'Token is missing!'}), 403
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
-            current_user = data['user']
-        except:
-            return jsonify({'message': 'Token is invalid!'}), 403
+            data = jwt.decode(token, secret_key, algorithms=["HS256"])
+            current_user = storage.query(User).filter_by(email=data['user']).first()
+        except Exception as e:
+            return jsonify({
+                'message': 'Token is invalid!',
+                'error': str(e)
+                }), 403
         return f(current_user, *args, **kwargs)
     return decorated
